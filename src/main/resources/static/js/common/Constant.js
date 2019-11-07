49bbbec8074ce81154995bca53c5efd6090d8f5e
lomofu <943034679@qq.com>
2019-11-08 02:53:43 +0800

    详情和修改

---
+++ src/main/resources/static/js/common/Constant.js
@@ -0,0 +1,16 @@
+const Constant = {
+
+    movieinfo: {
+        TITLE: '回复Ta',
+        TEXT_IS_EMPTY: '请不要不写回复内容呀'
+    },
+    user: {
+        DELETE_WARNING: '您确定要删除此评论？',
+        YES: '是的',
+        NO: '再想想'
+
+    }
+
+
+};
+
