c980552524c38172dd2bc077c5c134c672afa9e3
FULO2 <LOMO.FU@OOCL.COM>
2019-11-08 09:52:40 +0800

    代码优化

--- src/main/resources/static/js/common/Constant.js
+++ src/main/resources/static/js/common/Constant.js
@@ -1,16 +1,18 @@
-const Constant = {
+function getConstant() {
 
-    movieinfo: {
-        TITLE: '回复Ta',
-        TEXT_IS_EMPTY: '请不要不写回复内容呀'
-    },
-    user: {
-        DELETE_WARNING: '您确定要删除此评论？',
-        YES: '是的',
-        NO: '再想想'
+    const Constant = {
+        movieinfo: {
+            TITLE: '回复Ta',
+            TEXT_IS_EMPTY: '请不要不写回复内容呀'
+        },
+        user: {
+            DELETE_WARNING: '您确定要删除此评论？',
+            YES: '是的',
+            NO: '再想想'
 
-    }
+        }
+    };
+    return Constant;
+}
 
-
-};
 
